5f9c1ed28c3995ce8ab5df2b56fc87cf0ecfe150
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-02-26 00:34:29 +0100

    Switch to Brewfile for Homebrew install

---
+++ Brewfile
@@ -0,0 +1,83 @@
+# Brews (Packages)
+brew "awscli"
+brew "bat"
+brew "curl"
+brew "deno", link: false
+brew "diff-so-fancy"
+brew "exa"
+brew "fd"
+brew "fish"
+brew "fzf"
+brew "gcc"
+brew "gh"
+brew "git-delta"
+brew "git-flow"
+brew "git"
+brew "gitless"
+brew "glib"
+brew "go"
+brew "grc"
+brew "harfbuzz"
+brew "httpie"
+brew "hub"
+brew "jesseduffield/lazygit/lazygit"
+brew "mas"
+brew "moreutils"
+brew "nvie/tap/git-toolbelt"
+brew "pango"
+brew "python@3.9"
+brew "starship"
+brew "terraform"
+brew "thefuck"
+brew "tldr", link: false
+brew "tree"
+brew "typescript"
+brew "watchman"
+brew "wget"
+brew "yarn"
+brew "zsh-syntax-highlighting"
+
+# Casks (Applications)
+cask "alfred"
+cask "calibre"
+cask "docker"
+cask "firefox-developer-edition"
+cask "google-chrome"
+cask "iterm2"
+cask "microsoft-teams"
+cask "miro"
+cask "nordvpn"
+cask "notion"
+cask "obsidian"
+cask "safari-technology-preview"
+cask "slack-beta"
+cask "spotify"
+cask "transmission"
+cask "typora"
+cask "visual-studio-code-insiders"
+cask "whatsapp"
+cask "zoom"
+
+# App Store Applications
+mas "1Blocker", id: 1107421413
+mas "Adblock Plus", id: 1432731683
+mas "Bear", id: 1091189122
+mas "Boop", id: 1518425043
+mas "Bumpr", id: 1166066070
+mas "FileBot", id: 905384638
+mas "Grammarly for Safari", id: 1462114288
+mas "Magnet", id: 441258766
+mas "NewsGuard", id: 1438657064
+mas "NordVPN IKE", id: 1116599239
+mas "Raindrop.io for Safari", id: 957810159
+mas "Twitter", id: 1482454543
+
+# Taps
+tap "d12frosted/emacs-plus"
+tap "github/gh"
+tap "homebrew/bundle"
+tap "homebrew/cask-versions"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "jesseduffield/lazygit"
+tap "nvie/tap"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,95 +2,32 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# conditionally install a formula if it is not yet installed
-# brew list <formula1> || brew install <formula1>
-# brew list --cask <formula1> || brew install --cask <formula1>
+# TODO: Keep an eye out for a different `--no-quarantine` solution.
+# Currently, you can't do `brew bundle --no-quarantine` as an option.
+# export HOMEBREW_CASK_OPTS="--no-quarantine --no-binaries"
+# https://github.com/Homebrew/homebrew-bundle/issues/474
+
+# HOMEBREW_CASK_OPTS is exported in `zshenv` with
+# `--no-quarantine` and `--no-binaries` options,
+# which makes them available to Homebrew for the
+# first install (before our `zshrc` is sourced).
+
+brew bundle --verbose
 
 # ___DEVELOPMENT___
-
-# A good terminal
-brew list --cask iterm2 || brew install --cask iterm2
-brew list mas || brew install mas       # Mac App Store command-line interface
-brew list bat || brew install bat       # Clone of cat(1) with syntax highlighting and Git integration
-brew list httpie || brew install httpie # User-friendly cURL replacement (command-line HTTP client)
-# brew install tree
-# brew install wget
-# brew install tldr
-# brew install thefuck
-
 # Git-related
-
-# Dash
-# brew install --cask dash4
-
-# Text editors/IDEs
-brew list --cask visual-studio-code-insiders || brew install --cask --no-quarantine visual-studio-code-insiders
-
-# JetBrains
 # brew install --cask intellij-idea
-
 # Xcode
 # mas install 497799835 # Will take forever to download, yes.
-
-# AWS CLI
-# brew install awscli
-
 # Docker
 # brew install --cask docker
-
-# GoLang
-# brew install go
-
-# Python
-# brew install python3
-
-# DevOps
-# brew install terraform
-
 # Databases
-
-# VPN
-# brew install --cask nordvpn
-
-# ___PRODUCTIVITY___
-
-# Efficiency booster
-brew list --cask alfred || brew install --cask --no-quarantine alfred
-
-# Slack
-# brew install --cask slack-beta
-
-# Miro
-# brew install --cask miro
-
-# Notes & related
-# mas install 1091189122 # Bear
-# brew install --cask notion
-# brew install --cask obsidian
-# brew install --cask typora
-
-# Magnet
-# mas install 441258766
-
-# Bumpr
-# mas install 1166066070
-
-# ___BROWSERS___
-brew list --cask google-chrome || brew install --cask --no-quarantine google-chrome
-# brew list --cask safari-technology-preview || brew install --cask safari-technology-preview
-# brew install --cask firefox-developer-edition
-# brew install --cask raindropio
-
 # ___COMMON APPS___
-# brew install --cask 1password-beta
-# brew install --cask calibre
-# brew install --cask spotify
-# brew install --cask transmission
-# brew install --cask whatsapp
-# mas install 1482454543 # Twitter
-
-# Videoconferencing
-# brew install --cask microsoft-teams
-# brew install --cask zoom
+# brew list --cask 1password-beta || brew install --cask 1password-beta
