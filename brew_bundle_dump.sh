b4d42e1037958f300020ffa3cd51b8709465f4cb
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-03-03 22:13:24 +0100

    Add fish alias for dumping brew to `Brewfile`

--- brew_bundle_dump.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Write all installed casks/formulae/images/taps into a Brewfile in the current directory."
-
-brew bundle dump --force --describe --file=brew/Brewfile --verbose --cleanup
---
+++ fish/functions/brew_bundle_dump.fish
@@ -0,0 +1,4 @@
+function brew_bundle_dump --description 'Writing all installed casks/formulae/images/taps into a Brewfile in the current directory.' --wraps 'brew bundle dump'
+    echo "Writing all installed casks/formulae/images/taps into a Brewfile in the current directory."
+    command brew bundle dump --force --describe --file=~/.dotfiles/brew/Brewfile --verbose --cleanup $argv
+end
