9211103e034ce9f7d6ff917123864836bc191a60
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-03-05 23:23:09 +0100

    add repos

---
+++ repos/setup.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+COMMENT="\#*"
+REPO_PATH=$(realpath -m ~/repos)
+
+find * -name "*.list" | while read fn; do
+  folder="${fn%.*}"
+  echo "Cloning $folder repositories..." # echo type info
+
+  echo "Creating $folder folder..." # echo substep_info
+  mkdir -p "$REPO_PATH/$folder"
+
+  while read -r repo; do
+    if [[ $repo == "$COMMENT" ]]; then
+      continue
+    else
+      pushd "$REPO_PATH/$folder" &>/dev/null
+      git clone "$repo" &>/dev/null
+      if [[ $? -eq 128 ]]; then
+        # echo substep_success
+        echo "$repo already exists."
+      elif [[ $? -eq 0 ]]; then
+        # echo substep_success
+        echo "Cloned $repo."
+      else
+        # substep_error
+        echo "Failed to clone $repo."
+      fi
+      popd &>/dev/null
+    fi
+  done <"$fn"
+  # echo success
+  echo "Finished cloning $folder repositories."
+done
