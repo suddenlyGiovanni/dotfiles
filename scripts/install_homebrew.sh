5a966a05763d79655dd4d7d1f58873004dcfcb2a
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-03-19 21:50:24 +0100

    Refactor `setup_homebrew.sh`

---
+++ scripts/install_homebrew.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+echo "======================================================"
+echo "Installing Homebrew, the OSX package manager...If it's"
+echo "already installed, this will do nothing."
+echo "======================================================"
+
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists brew; then
+  echo "brew exists, version: $(brew --version)"
+else
+  echo "brew doesn't exist, hold tight while we fetch it for you."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
--- scripts/setup_homebrew.sh
+++ scripts/setup_homebrew.sh
@@ -1,37 +1,8 @@
 #!/usr/bin/env sh
 
-printf "\n<<< Starting Homebrew Setup >>>\n"
-
-exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-if exists brew; then
-  echo "brew exists, skipping install"
-else
-  echo "brew doesn't exist, continuing with install"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# TODO: Keep an eye out for a different `--no-quarantine` solution.
-# Currently, you can't do `brew bundle --no-quarantine` as an option.
-# export HOMEBREW_CASK_OPTS="--no-quarantine --no-binaries"
-# https://github.com/Homebrew/homebrew-bundle/issues/474
-
-# HOMEBREW_CASK_OPTS is exported in `zshenv` with
-# `--no-quarantine` and `--no-binaries` options,
-# which makes them available to Homebrew for the
-# first install (before our `zshrc` is sourced).
+echo "======================================================"
+echo "Setting up Homebrew packages...                       "
+echo "There may be some warnings.                           "
+echo "======================================================"
 
 brew bundle --verbose --file=brew/Brewfile
-
-# ___DEVELOPMENT___
-# Git-related
-# brew install --cask intellij-idea
-# Xcode
-# mas install 497799835 # Will take forever to download, yes.
-# Docker
-# brew install --cask docker
-# Databases
-# ___COMMON APPS___
-# brew list --cask 1password-beta || brew install --cask 1password-beta
