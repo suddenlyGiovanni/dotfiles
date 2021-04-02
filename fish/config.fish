360d91d15d3dfe82669f200e2cf49acb5a41e438
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-04-02 16:16:22 +0200

    Fix use updated `curl` from brew formula

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,8 @@ source ~/.iterm2_shell_integration.(basename $SHELL)
 source ~/.config/fish/git-abbreviations.fish
 
 set -x PATH (pwd)"/git-fuzzy/bin:$PATH"
-
 set -gx STARSHIP_CONFIG '~/.config/starship.toml'
+set -g fish_user_paths /usr/local/opt/curl/bin $fish_user_paths
 
 # boo-tup the `spacefish` prompt
 starship init fish | source
