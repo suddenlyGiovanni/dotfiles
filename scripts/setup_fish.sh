a493ab779ec49721c369b00d4a4a917e5b7361ce
--get <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-09-07 16:44:15 +0200

    initial config apple silicon

--- scripts/setup_fish.sh
+++ scripts/setup_fish.sh
@@ -1,20 +1,19 @@
-#! /usr/bin/env sh
-
+#! /usr/bin/env bash
 # installation is unnecessary: it's in the Brewfile
 
 # Switch to using brew-installed bash as default shell https://stackoverflow.com/a/4749368/1341838
-if grep --fixed-strings --line-regexp --quiet "/usr/local/bin/zsh" "/etc/shells"; then
+if grep --fixed-strings --line-regexp --quiet "$(which fish)" "/etc/shells"; then
   # code if found
-  echo "/usr/local/bin/fish already exists in /etc/shells"
+  echo "$(which fish) already exists in /etc/shells"
 else
   # code if not found
   echo "Enter superuser (sudo) password to edit /etc/shells"
-  echo "/usr/local/bin/fish" | sudo tee -a "/etc/shells" >/dev/null
+  echo $(which fish) | sudo tee -a "/etc/shells" >/dev/null
 fi
 
-if [ "$SHELL" = '/usr/local/bin/fish' ]; then
-  echo "$SHELL is already /usr/local/bin/fish"
+if [ "$0" == "fish" ]; then
+  echo "$SHELL is already $(which fish)"
 else
   echo "Enter user password to change login shell"
-  chsh -s "/usr/local/bin/fish"
+  chsh -s "$(which fish)"
 fi
