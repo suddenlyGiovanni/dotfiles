ab581c4d63cacb17845c3535f004fb3408bdd6f3
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-03 16:14:31 +0100

    Update gh config and brew dependencies
    The version number has been added to the gh config file.
    Substantial changes have been made to the Brewfile, with numerous
    packages and casks removed or updated. This includes changes like
    replacing "visual-studio-code-insiders" with "visual-studio-code", and
    removing packages including "python@3.11", "docker-compose", and "ruby".
    Various comments have also been updated to reflect these changes.

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,13 @@
-function ls --description 'List contents of directory using long format' --wraps exa
-    command exa --all --long --icons --header --classify --group-directories-first --sort=type --time-style=long-iso --git $argv
+function ls --description 'List contents of directory using long format' --wraps eza
+    command eza
+      --all
+      --long
+      --icons
+      --header
+      --classify
+      --group-directories-first
+      --sort=type
+      --time-style=long-iso
+      --git
+      $argv
 end
