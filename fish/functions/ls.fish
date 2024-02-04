33aca4991250480cb2fbf7f831bf3921608a680f
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-04 20:53:18 +0100

    Simplify 'ls' function command in ls.fish
    The 'ls' command in the fish shell function 'ls.fish' has been simplified. The arguments remain unchanged, but are now passed in a single line to the 'eza' command instead of being passed individually. Two new arguments, '--hyperlink' and '--git-repos', have also been added.

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,13 +1,3 @@
 function ls --description 'List contents of directory using long format' --wraps eza
-    command eza
-      --all
-      --long
-      --icons
-      --header
-      --classify
-      --group-directories-first
-      --sort=type
-      --time-style=long-iso
-      --git
-      $argv
+    command eza --all --long --icons --header --classify --group-directories-first --sort=type --time-style=long-iso --hyperlink --git --git-repos $argv
 end
