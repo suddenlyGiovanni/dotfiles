a088d905c0f7a01bf70791621ed296b4f327f6ef
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-06 14:02:02 +0100

    Refactor environment configuration structure
    Consolidate the `systemPackages` and `pathsToLink` into a single `environment` attribute set for better organization and clarity. This change maintains existing functionality while making the configuration more structured and readable.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -22,41 +22,44 @@
         {
           ids.gids.nixbld = 350;
 
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = with pkgs; [
-            vim
-            nixd # nix lsp deamon
-            nixfmt-rfc-style # nix lang formatter
-            colima # Container runtimes with minimal setup
-            awscli2 # Unified tool to manage your AWS services
-            bat # A cat(1) clone with syntax highlighting and Git integration
-            coreutils # The GNU Core Utilities
-            docker # An open source project to pack, ship and run any application as a lightweight container
-            docker-buildx # Docker CLI plugin for extended build capabilities with BuildKit
-            lazydocker # A simple terminal UI for both docker and docker-compose
-            docker-slim # Minify and secure Docker containers
-            eza # A modern, maintained replacement for ls
-            fd # A simple, fast and user-friendly alternative to find
-            flyctl # Command line tools for fly.io services
-            fnm # Fast and simple Node.js version manager
-            fzf # Command-line fuzzy finder written in Go
-            gh # GitHub CLI tool
-            git # Distributed version control system
-            glow # Render markdown on the CLI, with pizzazz!
-            httpie # A command line HTTP client whose goal is to make CLI human-friendly
-            jq # A lightweight and flexible command-line JSON processor
-            less # A more advanced file pager than 'more'
-            nushell # A modern shell written in Rust
-            shellcheck # Shell script analysis tool
-            shfmt # A shell parser and formatter
-            starship # A minimal, blazing fast, and extremely customizable prompt for any shell
-            wget # Tool for retrieving files using HTTP, HTTPS, and FTP
-            zoxide # A fast cd command that learns your habits
-            bun # Incredibly fast JavaScript runtime, bundler, transpiler and package manager – all in one
-          ];
+          environment = {
+            # List packages installed in system profile. To search by name, run:
+            # $ nix-env -qaP | grep wget
+            systemPackages = with pkgs; [
+              vim
+              nixd # nix lsp deamon
+              nixfmt-rfc-style # nix lang formatter
+              colima # Container runtimes with minimal setup
+              awscli2 # Unified tool to manage your AWS services
+              bat # A cat(1) clone with syntax highlighting and Git integration
+              coreutils # The GNU Core Utilities
+              docker # An open source project to pack, ship and run any application as a lightweight container
+              docker-buildx # Docker CLI plugin for extended build capabilities with BuildKit
+              lazydocker # A simple terminal UI for both docker and docker-compose
+              docker-slim # Minify and secure Docker containers
+              eza # A modern, maintained replacement for ls
+              fd # A simple, fast and user-friendly alternative to find
+              flyctl # Command line tools for fly.io services
+              fnm # Fast and simple Node.js version manager
+              fzf # Command-line fuzzy finder written in Go
+              gh # GitHub CLI tool
+              git # Distributed version control system
+              glow # Render markdown on the CLI, with pizzazz!
+              httpie # A command line HTTP client whose goal is to make CLI human-friendly
+              jq # A lightweight and flexible command-line JSON processor
+              less # A more advanced file pager than 'more'
+              nushell # A modern shell written in Rust
+              shellcheck # Shell script analysis tool
+              shfmt # A shell parser and formatter
+              starship # A minimal, blazing fast, and extremely customizable prompt for any shell
+              wget # Tool for retrieving files using HTTP, HTTPS, and FTP
+              zoxide # A fast cd command that learns your habits
+              bun # Incredibly fast JavaScript runtime, bundler, transpiler and package manager – all in one
+            ];
 
-          environment.pathsToLink = [ "/share/zsh" ]; # List of directories to be symlinked in /run/current-system/sw.
+            pathsToLink = [ "/share/zsh" ]; # List of directories to be symlinked in /run/current-system/sw.
+
+          };
 
           # Auto upgrade nix package and the daemon service.
           services.nix-daemon.enable = true;
