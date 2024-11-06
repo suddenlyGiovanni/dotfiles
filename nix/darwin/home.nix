baf354d9cc0a24f9fc211bd8e157f57030f9ac79
suddenlyGiovanni <15946771+suddenlygiovanni@users.noreply.github.com>
2024-11-06 14:34:36 +0100

    Enable zsh autosuggestions and improve history management.
    Added configuration to enable zsh autosuggestions with customizable options. Enhanced zsh history settings, increasing size to 10000 lines and setting a custom file path. Removed manual sourcing of zsh configurations from home manager.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -24,9 +24,6 @@
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     file = {
-      ".zshrc" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/Users/suddenlygiovanni/dotfiles/zshrc/.zshrc";
-      };
       ".config/nix/nix.conf" = {
         source = config.lib.file.mkOutOfStoreSymlink "/Users/suddenlygiovanni/dotfiles/nix/nix.conf";
       };
--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -11,6 +11,20 @@
     switch = "darwin-rebuild switch --flake ~/dotfiles/nix/darwin";
   };
   enableCompletion = true; # Enable zsh completion.
+
+  autosuggestion = {
+    enable = true; # Enable zsh autosuggestions
+    highlight = null; # Custom styles for autosuggestion highlighting.
+    strategy = [ "history" ]; # an array that specifies how suggestions should be generated.
+  };
+
+  # Options related to commands history configuration.
+  history = {
+    append = false; # If set, zsh sessions will append their history list to the history file, rather than replace it.
+    size = 10000; # Number of history lines to keep.
+    path = "${config.xdg.dataHome}/zsh/history"; # History file location
+  };
+
   initExtra = ''
     # Add any additional configurations here
     export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
@@ -19,4 +33,13 @@
     fi
   '';
 
+  # Plugins to source in {file}`.zshrc`.
+  plugins = [
+    #    {
+    #      # will source zsh-autosuggestions.plugin.zsh
+    #      name = "zsh-autosuggestions";
+    #      src = pkgs.zsh-autosuggestions;
+    #      file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+    #    }
+  ];
 }
