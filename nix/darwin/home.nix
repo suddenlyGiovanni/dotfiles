8deba4bbc8a309fd047aac1dff092a0ffb08fcf2
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-06 01:48:05 +0100

    Add Git aliases to home.nix
    Introduced various Git command aliases for improved efficiency and convenience. These include shortcuts for clone, add, commit, log, fetch, pull, push, rebase, and stash commands among others.

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -51,6 +51,83 @@
       enable = true;
       userName = "suddenlyGiovanni"; # Default user name to use.
       userEmail = "15946771+suddenlyGiovanni@users.noreply.github.com"; # Default user email to use.
+      aliases = {
+        # clone
+        clone = "clone --filter=blob:none"; # clones a repo while only downloading the required blobs
+
+        # add
+        a = "add -p"; # add
+        chunkyadd = "add --patch"; # stage commits chunk by chunk
+
+        # Shows list of contributors of a repository.
+        contributors = "shortlog --summary --numbered --email";
+
+        # branch
+        b = "branch --verbose"; # branch (verbose)
+
+        # commit
+        c = "commit --message"; # commit with message
+        ca = "commit --all --message"; # commit all with message
+        ci = "commit"; # commit
+        amend = "commit --amend"; # ammend your last commit
+
+        # checkout
+        co = "checkout"; # switch branches or restore working tree files
+        nb = "checkout -b"; # create and switch to a new branch (mnemonic: "git new branch branchname...")
+
+        # cherry-pick
+        cp = "cherry-pick -x"; # Apply the changes introduced by some existing commits (grab a change from a branch)
+
+        # diff
+        d = "diff"; # diff unstaged changes
+        dc = "diff --cached"; # diff staged changes
+        last = "diff HEAD^"; # diff last committed change
+
+        # log
+        l = "log --graph --date=short";
+        lg = "log --color --graph --date=iso --pretty=format:'%C(yellow)%h%C(reset) %C(green)%ai%x08%x08%x08%x08%x08%x08%x08%x08%x08%C(reset) %<(20)%C(blue)%an%C(reset) %C(red)%d%C(reset) %s' --abbrev-commit";
+        changes = "log --pretty=format:\"%h %cr %cn %Cgreen%s%Creset\" --name-status";
+        short = "log --pretty=format:\"%h %cr %cn %Cgreen%s%Creset\"";
+        simple = "log --pretty=format:\" * %s\"";
+        shortnocolor = "log --pretty=format:\"%h %cr %cn %s\"";
+
+        # fetch
+        f = "fetch --prune"; # Download objects and refs from another repository (Before fetching, remove any remote-tracking references that no longer exist on the remote)
+
+        # pull
+        pl = "pull"; # pull
+
+        # push
+        ps = "push --set-upstream"; # push
+
+        # rebase
+        rc = "rebase --continue"; # continue rebase
+        rs = "rebase --skip"; # skip rebase
+
+        # remote
+        r = "remote -v"; # show remotes (verbose)
+
+        # reset
+        unstage = "reset HEAD"; # remove files from index (tracking)
+        uncommit = "reset --soft HEAD^"; # go back before last commit, with files in uncommitted state
+        filelog = "log -u"; # show changes to a file
+        mt = "mergetool"; # fire up the merge tool
+
+        # stash
+        ss = "stash"; # stash changes
+        sl = "stash list"; # list stashes
+        sa = "stash apply"; # apply stash (restore changes)
+        sd = "stash drop"; # drop stashes (destory changes)
+
+        # status
+        s = "status"; # status
+        st = "status -sb"; # status branch
+        stat = "status"; # status
+
+        # tag
+        t = "tag -n"; # show tags with <n> lines of each tag message
+
+      };
       ignores = [ ".DS_Store" ];
     };
 
