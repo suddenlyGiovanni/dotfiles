4b8510ba63faa5d4d767b014b58dcea92af787af
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-06 21:43:21 +0100

    Uncomment zoxide command definitions
    This commit restores the default z and zi command definitions by un-commenting the "--no-cmd" option from the zoxide configuration. This change allows zoxide to define its default commands, providing users with the intended functionality.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- nix/home/zoxide.nix
+++ nix/home/zoxide.nix
@@ -7,7 +7,7 @@
   enable = true;
   package = pkgs.zoxide; # Zoxide package to install.
   options = [
-    "--no-cmd" # Prevents zoxide from defining the z and zi commands.
+    #    "--no-cmd" # Prevents zoxide from defining the z and zi commands.
     "--cmd cd" # Replace the cd command.
   ]; # List of options to pass to zoxide init.
   enableBashIntegration = false; # Whether to enable Bash integration.
