d4676da88734a8f679bf9286da12e72fa4d2ee1d
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-15 12:32:30 +0100

    Remove redundant PATH export in zsh configuration
    This change removes an unnecessary export statement for the PATH variable in the zsh configuration file. The system profiles and environment initialization scripts handle PATH setup, making this line redundant. Removing it helps maintain cleaner and more maintainable configuration files.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -42,7 +42,6 @@
     printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "zsh"}}\x9c'
 
     # Add any additional configurations here
-    export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
     if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
     fi
