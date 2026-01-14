12a6afc6cc4b0a48cdedc202c004a48e35cbdea1
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2026-01-14 12:49:08 +0100

    fix(fish): fix shell initialization and add home-manager completions path
    - Remove duplicate enable_transience call from shellInitLast
      (already called by starship integration with enableTransience = true)
    - Add home-manager vendor_completions.d to fish_complete_path
      This ensures completions from home-manager packages (like bun) are found

--- nix/home/programs/shell/fish.nix
+++ nix/home/programs/shell/fish.nix
@@ -7,7 +7,11 @@
     shellAliases = {}; # An attribute set that maps aliases (the top level attribute names in this option) to command strings or directly to build outputs.
     shellAbbrs = {}; # An attribute set that maps aliases (the top level attribute names in this option) to abbreviations. Abbreviations are expanded with the longer phrase after they are entered.
     preferAbbrs = true; # If enabled, abbreviations will be preferred over aliases when other modules define aliases for fish.
-    shellInit = ""; # Shell script code called during fish shell initialisation.
+    # Add home-manager vendor completions to fish_complete_path
+    # This ensures completions from packages installed via home-manager (like bun) are found
+    shellInit = ''
+      set -gp fish_complete_path ~/.local/state/nix/profiles/home-manager/home-path/share/fish/vendor_completions.d
+    '';
     loginShellInit = ""; # Shell script code called during fish login shell initialisation.
 
     # Shell script code called during interactive fish shell initialisation.
@@ -19,9 +23,8 @@
     '';
 
     # Shell script code called during interactive fish shell initialisation, this will be the last thing executed in fish startup.
-    shellInitLast = ''
-      enable_transience
-    '';
+    # Note: enable_transience is already called by starship integration (enableTransience = true in starship.nix)
+    shellInitLast = "";
     plugins = []; # The plugins to source in {file}`conf.d/99plugins.fish`.
     functions = {}; # Basic functions to add to fish. For more information see <https://fishshell.com/docs/current/cmds/function.html>.
   };
